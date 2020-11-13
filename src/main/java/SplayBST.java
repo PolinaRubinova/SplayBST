889e479373333e2ae01e1cc92ea08e706a35d482
PolinaRubinova <jerrygonsales@mail.ru>
2020-11-14 01:15:29 +0300

    визуализация

--- src/main/java/SplayBST.java
+++ src/main/java/SplayBST.java
@@ -188,72 +188,70 @@ public class SplayBST<T extends Comparable<T>> extends AbstractSet<T> {
         printNodeInternal(Collections.singletonList(root), 1, maxLevel);
     }
 
-        private static <T extends Comparable<?>> void printNodeInternal(List<Node<T>> nodes, int level, int maxLevel) {
-            if (nodes.isEmpty() || isAllElementsNull(nodes)) return;
+    private static <T extends Comparable<?>> void printNodeInternal(List<Node<T>> nodes, int level, int maxLevel) {
+        if (nodes.isEmpty() || isAllElementsNull(nodes)) return;
 
-            int floor = maxLevel - level;
-            int lines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
-            int firstSpaces = (int) Math.pow(2, (floor)) - 1;
-            int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+        int floor = maxLevel - level;
+        int lines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+        int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+        int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
 
-            printWhitespaces(firstSpaces);
+        printWhitespaces(firstSpaces);
+        ArrayList<Node<T>> newNodes = new ArrayList<>();
 
-            ArrayList<Node<T>> newNodes = new ArrayList<>();
+        for (Node<T> node : nodes) {
+            if (node != null) {
+                System.out.print(node.value);
+                newNodes.add(node.left);
+                newNodes.add(node.right);
+            } else {
+                newNodes.add(null);
+                newNodes.add(null);
+                System.out.print(" ");
+            }
+            printWhitespaces(betweenSpaces);
+        }
+        System.out.println();
 
+        for (int i = 1; i <= lines; i++) {
             for (Node<T> node : nodes) {
-                if (node != null) {
-                    System.out.print(node.value);
-                    newNodes.add(node.left);
-                    newNodes.add(node.right);
-                } else {
-                    newNodes.add(null);
-                    newNodes.add(null);
-                    System.out.print(" ");
+                printWhitespaces(firstSpaces - i);
+                if (node == null) {
+                    printWhitespaces(lines + lines + i + 1);
+                    continue;
                 }
 
-                printWhitespaces(betweenSpaces);
+                if (node.left != null) {
+                    System.out.print("/");
+                } else printWhitespaces(1);
+
+                printWhitespaces(i + i - 1);
+
+                if (node.right != null) {
+                    System.out.print("\\");
+                } else printWhitespaces(1);
+
+                printWhitespaces(lines + lines - i);
             }
-            System.out.println("");
-
-            for (int i = 1; i <= lines; i++) {
-                for (Node<T> node : nodes) {
-                    printWhitespaces(firstSpaces - i);
-                    if (node == null) {
-                        printWhitespaces(lines + lines + i + 1);
-                        continue;
-                    }
-
-                    if (node.left != null) {
-                        System.out.print("/");
-                    } else printWhitespaces(1);
-
-                    printWhitespaces(i + i - 1);
-
-                    if (node.right != null) {
-                        System.out.print("\\");
-                    } else printWhitespaces(1);
-
-                    printWhitespaces(lines + lines - i);
-                }
-                System.out.println("");
-            }
-            printNodeInternal(newNodes, level + 1, maxLevel);
+            System.out.println();
         }
+        printNodeInternal(newNodes, level + 1, maxLevel);
+    }
 
-        private static void printWhitespaces(int count) {
-            for (int i = 0; i < count; i++) System.out.print(" ");
-        }
+    private static void printWhitespaces(int count) {
+        for (int i = 0; i < count; i++) System.out.print(" ");
+    }
 
-        private static <T extends Comparable<?>> int maxLevel(Node<T> node) {
-            return (node == null) ? 0 :
-                    Math.max(maxLevel(node.left), maxLevel(node.right)) + 1;
-        }
+    private static <T extends Comparable<?>> int maxLevel(Node<T> node) {
+        return (node == null) ? 0 :
+                Math.max(maxLevel(node.left), maxLevel(node.right)) + 1;
+    }
 
-        private static <T> boolean isAllElementsNull(List<T> list) {
-            for (Object object : list) {
-                if (object != null)
-                    return false;
-            }
-            return true;
+    private static <T> boolean isAllElementsNull(List<T> list) {
+        for (Object object : list) {
+            if (object != null)
+                return false;
         }
+        return true;
+    }
 }
