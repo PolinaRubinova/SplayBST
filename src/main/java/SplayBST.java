ffef232a4289c37e0d68089d9fc82a8392638fbe
PolinaRubinova <jerrygonsales@mail.ru>
2020-11-14 01:06:28 +0300

    визуализация

--- src/main/java/SplayBST.java
+++ src/main/java/SplayBST.java
@@ -1,7 +1,6 @@
 import org.jetbrains.annotations.NotNull;
 
-import java.util.AbstractSet;
-import java.util.Iterator;
+import java.util.*;
 
 public class SplayBST<T extends Comparable<T>> extends AbstractSet<T> {
 
@@ -140,8 +139,26 @@ public class SplayBST<T extends Comparable<T>> extends AbstractSet<T> {
 
     @NotNull
     @Override
-    public Iterator<T> iterator() { //**********************************************************************************
-        return null;
+    public Iterator<T> iterator() {
+        return new SplayBSTIterator();
+    }
+
+    public class SplayBSTIterator implements Iterator<T> { //***********************************************************
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public T next() {
+            return null;
+        }
+
+        @Override
+        public void remove() {
+
+        }
     }
 
 
@@ -166,19 +183,77 @@ public class SplayBST<T extends Comparable<T>> extends AbstractSet<T> {
         return x;
     }
 
-    public void printTree () {
-        if (isEmpty())
-            System.out.println("Empty tree");
-        else
-            printTree(root);
+    public void printTree() {
+        int maxLevel = maxLevel(root);
+        printNodeInternal(Collections.singletonList(root), 1, maxLevel);
     }
 
-    private void printTree (Node<T> node) {
-        if (node == null)
-            return;
+        private static <T extends Comparable<?>> void printNodeInternal(List<Node<T>> nodes, int level, int maxLevel) {
+            if (nodes.isEmpty() || isAllElementsNull(nodes)) return;
 
-        printTree(node.left);
-        System.out.println(node.value);
-        printTree(node.right);
-    }
+            int floor = maxLevel - level;
+            int lines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+            int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+            int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+            printWhitespaces(firstSpaces);
+
+            ArrayList<Node<T>> newNodes = new ArrayList<>();
+
+            for (Node<T> node : nodes) {
+                if (node != null) {
+                    System.out.print(node.value);
+                    newNodes.add(node.left);
+                    newNodes.add(node.right);
+                } else {
+                    newNodes.add(null);
+                    newNodes.add(null);
+                    System.out.print(" ");
+                }
+
+                printWhitespaces(betweenSpaces);
+            }
+            System.out.println("");
+
+            for (int i = 1; i <= lines; i++) {
+                for (Node<T> node : nodes) {
+                    printWhitespaces(firstSpaces - i);
+                    if (node == null) {
+                        printWhitespaces(lines + lines + i + 1);
+                        continue;
+                    }
+
+                    if (node.left != null) {
+                        System.out.print("/");
+                    } else printWhitespaces(1);
+
+                    printWhitespaces(i + i - 1);
+
+                    if (node.right != null) {
+                        System.out.print("\\");
+                    } else printWhitespaces(1);
+
+                    printWhitespaces(lines + lines - i);
+                }
+                System.out.println("");
+            }
+            printNodeInternal(newNodes, level + 1, maxLevel);
+        }
+
+        private static void printWhitespaces(int count) {
+            for (int i = 0; i < count; i++) System.out.print(" ");
+        }
+
+        private static <T extends Comparable<?>> int maxLevel(Node<T> node) {
+            return (node == null) ? 0 :
+                    Math.max(maxLevel(node.left), maxLevel(node.right)) + 1;
+        }
+
+        private static <T> boolean isAllElementsNull(List<T> list) {
+            for (Object object : list) {
+                if (object != null)
+                    return false;
+            }
+            return true;
+        }
 }
--- src/test/java/TestSplayBST.java
+++ src/test/java/TestSplayBST.java
@@ -1,46 +1,45 @@
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class TestSplayBST {
 
         @Test
-        public void splayTree () {
+        public void splayTree() {
                 SplayBST<Integer> actual = new SplayBST<Integer>();
                 SplayBST<Integer> control = new SplayBST<Integer>();
 
-                actual.add(25);
-                actual.add(30);
-                actual.add(13);
-                actual.add(20);
-                actual.add(18);
                 actual.add(15);
-                actual.add(24);
-                actual.add(12);
-                actual.add(16);
+                actual.add(3);
+                actual.add(2);
                 actual.add(5);
-                control.add(25);
-                control.add(30);
-                control.add(13);
-                control.add(20);
-                control.add(18);
+                actual.add(10);
+                actual.add(1);
+
+                control.add(1);
                 control.add(15);
-                control.add(24);
-                control.add(12);
-                control.add(16);
+                control.add(3);
                 control.add(5);
+                control.add(10);
+                control.add(2);
+
                 actual.printTree();
-                actual.remove(12);
-                System.out.println("*");
-                actual.printTree();
-                actual.remove(25);
-                System.out.println("*");
-                actual.printTree();
-                actual.add(40);
-                System.out.println("*");
+                control.printTree();
+                System.out.println(actual == control);
+
+                actual.remove(10);
                 actual.printTree();
                 System.out.println(actual == control);
-                //Assert.assertEquals(actual, control);
-                //splayTree.remove(1);
-        }
 
+                actual.remove(2);
+                actual.printTree();
+                System.out.println(actual == control);
+
+                actual.add(11);
+                actual.add(12);
+                actual.printTree();
+                System.out.println(actual == control);
+
+                System.out.println(actual.contains(2));
+                actual.printTree();
+                control.printTree();
+        }
 }
