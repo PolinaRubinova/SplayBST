3747082b056ec3d9f6ad62f30be320e03c0c9c62
PolinaRubinova <jerrygonsales@mail.ru>
2020-11-24 20:31:07 +0300

    split, merge и тп

--- src/main/java/SplayBST.java
+++ src/main/java/SplayBST.java
@@ -1,10 +1,9 @@
-import org.jetbrains.annotations.NotNull;
-
 import java.util.*;
+import org.jetbrains.annotations.NotNull;
 
 public class SplayBST<T extends Comparable<T>> extends AbstractSet<T> {
 
-    private static class Node<T> {
+    public static class Node<T> {
         T value;
         Node<T> left = null;
         Node<T> right = null;
@@ -14,11 +13,20 @@ public class SplayBST<T extends Comparable<T>> extends AbstractSet<T> {
         }
     }
 
-    private Node<T> root = null;
+    public Node<T> root = null;
+
+    public Object getRootValue() {
+        return root.value;
+    }
 
     @Override
     public int size() {
         return size(root);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return root == null;
     }
 
     // Рекурсивно находим размер дерева
@@ -33,25 +41,121 @@ public class SplayBST<T extends Comparable<T>> extends AbstractSet<T> {
     // он стал корнем. Поэтому, если find вернул что угодно
     // (null или другой элемент), кроме нашего элемента -> false
 
-    public  boolean contains(T element) {
-        return find(element) != null &&
-                Objects.requireNonNull(find(element)).value == element;
+    @Override
+    public boolean contains(Object object) {
+        return find((T) object) != null &&
+                Objects.requireNonNull(find((T) object)).value == object;
     }
-
 
     // Эта функция возвращает новый корень Splay Tree
     // Если элемент присутствует в дереве, он перемещается в корень.
 
-    private Node<T> find(T value) {
+    public Node<T> find(T element) {
         if (root == null) return null;
-        return root = splay(root, value);
+        return root = splay(root, find(root, element).value);
+    }
+
+    // Эта операция выполняется как для обычного
+    // бинарного дерева поиска, только после нее
+    // запускается операция splay.
+
+    private Node<T> find(Node<T> start, T element){
+        int comparison = element.compareTo(start.value);
+        if (comparison == 0) {
+            return start;
+        } else if (comparison < 0) {
+            if (start.left == null) return start;
+            return find(start.left, element);
+        } else {
+            if (start.right == null) return start;
+            return find(start.right, element);
+        }
+    }
+
+
+    public class Pair<U, V> {
+        public Node<T> left;
+        public Node<T> right;
+
+        Pair(Node<T> left, Node<T> right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // Возвращаем два дерева, полученные отсечением правого или левого
+    // поддерева от корня, в зависимости от того, содержит корень
+    // элемент больше или не больше, чем element.
+
+    public Pair<Node<T>, Node<T>> split (T element) {
+        if (root == null) return new Pair<>(null, null);
+
+        Node<T> left = new Node<>(null);
+        Node<T> right = new Node<>(null);
+
+        root = splay(root, element);
+        int comparison = element.compareTo(root.value);
+
+        if (comparison > 0) {
+            if (root.right != null) right = root.right;
+            root.right = null;
+            left = root;
+
+        } else {
+            if (root.left != null) left = root.left;
+            root.left = null;
+            right = root;
+        }
+
+        return new Pair<>(left, right);
+    }
+
+    // Находим максимальное -> делаем его корнем
+
+    public Node<T> findMax(Node<T> start) {
+        if (start == null) return null;
+        while (start.right != null) {
+            start = splay(start, start.right.value);
+        }
+        return start;
+    }
+
+    // Находим минимальное -> делаем его корнем
+
+    public Node<T> findMin(Node<T> start) {
+        if (start == null) return null;
+        while (start.left != null) {
+            start = splay(start, start.left.value);
+        }
+        return start;
+    }
+
+    // Для слияния деревьев leftTree и rightTree, в которых все ключи
+    // leftTree меньше ключей в rightTree, запускаем splay от
+    // самого большого элемента в дереве tree1 (функция findMax).
+
+    // После этого корень tree1 содержит максимальный элемент,
+    // при этом у него нет правого ребёнка.
+    // Делаем tree2 правым поддеревом i и возвращаем полученное дерево.
+
+    public Node<T> merge (Node<T> left, Node<T> right) {
+        if (left == null && right == null) return null;
+        else if (left == null) return right;
+        else if (right == null) return left;
+
+        left = findMax(left);
+        right = findMin(right);
+        left.right = right;
+        root = left;
+        return root;
     }
 
 
     // Добавление элемента в дерево
 
-    // Если элемента нет в множестве, функция добавляет его в дерево и возвращает true.
-    // В ином случае функция оставляет множество нетронутым и возвращает false.
+    // Если элемента нет в множестве, функция добавляет его в
+    // дерево и возвращает true. В ином случае функция оставляет
+    // множество нетронутым и возвращает false.
 
     @Override
     public boolean add(T element) {
@@ -93,6 +197,26 @@ public class SplayBST<T extends Comparable<T>> extends AbstractSet<T> {
         return true;
     }
 
+    // Запускаем split(tree, element), который нам возвращает
+    // деревья tree1 и tree2, их подвешиваем к element
+    // как левое и правое поддеревья соответственно.
+
+    public boolean addWithSplit(T element) {
+        if (root == null) {
+            root = new Node<>(element);
+            return true;
+        }
+        if (root.value == element) return false;
+
+        Pair splitRes = split(element);
+        root = new Node<>(element);
+
+        if (splitRes.left.value != null) root.left = splitRes.left;
+        if (splitRes.right.value != null) root.right = splitRes.right;
+        return true;
+    }
+
+
     // Функция удаления элемента из дерева
 
     @Override
@@ -123,24 +247,38 @@ public class SplayBST<T extends Comparable<T>> extends AbstractSet<T> {
     }
 
 
-    // Эта функция помещает ключ в корень, если элемент
-    // присутствует в дереве. Если ключ отсутствует,
+    //Находим элемент в дереве, делаем Splay для него,
+    // делаем текущим деревом Merge его детей.
+
+    public boolean removeWithSplit(T element) {
+        if (root == null) return false;
+
+        root = splay(root, element);
+        if (root.value != element) return false;
+
+        root = merge(root.left, root.right);
+        return true;
+    }
+
+
+    // Эта функция помещает элемент в корень, если элемент
+    // присутствует в дереве. Если элемент отсутствует,
     // он переносит последний доступный элемент в корневой каталог.
     // Эта функция изменяет дерево и возвращает новый корень.
 
     private Node<T> splay(Node<T> node, T element) {
         if (node == null || node.value == element) return node;
 
-        //Если ключ находится в левом поддереве
+        //Если элемент находится в левом поддереве
         int cmp1 = element.compareTo(node.value);
         if (cmp1 < 0) {
-            // Если ключ не в дереве -> мы закончили
+            // Если элемент не в дереве -> мы закончили
             if (node.left == null) return node;
 
             // Zig-Zig (Left Left)
             int cmp2 = element.compareTo(node.left.value);
             if (cmp2 < 0) {
-                // Сначала рекурсивно приведем ключ как корень left-left
+                // Сначала рекурсивно приведем элемент как корень left-left
                 node.left.left = splay(node.left.left, element);
                 // Сделаем первое вращение для корня,
                 // второе вращение выполняется после else
@@ -149,7 +287,7 @@ public class SplayBST<T extends Comparable<T>> extends AbstractSet<T> {
 
             // Zig-Zag (Left Right)
             else if (cmp2 > 0) {
-                // Сначала рекурсивно приведем ключ как корень left-right
+                // Сначала рекурсивно приведем элемент как корень left-right
                 node.left.right = splay(node.left.right, element);
                 // Сделаем первое вращение для root.left
                 if (node.left.right != null)
@@ -160,15 +298,15 @@ public class SplayBST<T extends Comparable<T>> extends AbstractSet<T> {
             return  (node.left == null) ? node : rotateRight(node);
         }
 
-        //Если ключ находится в правом поддереве
+        //Если элемент находится в правом поддереве
         else {
-            // Если ключ не в дереве -> мы закончили
+            // Если элемент не в дереве -> мы закончили
             if (node.right == null) return node;
 
             // Zag-Zig (Right Left)
             int cmp2 = element.compareTo(node.right.value);
             if (cmp2 < 0) {
-                // Приведем ключ как корень right-left
+                // Приведем элемент как корень right-left
                 node.right.left  = splay(node.right.left, element);
                 // Сделаем первое вращение для root.right
                 if (node.right.left != null)
@@ -177,7 +315,7 @@ public class SplayBST<T extends Comparable<T>> extends AbstractSet<T> {
 
             // Zag-Zag (Right Right)
             else if (cmp2 > 0) {
-                // Приведем ключ как корень right-right
+                // Приведем элемент как корень right-right
                 // и сделаем первое вращение
                 node.right.right = splay(node.right.right, element);
                 node = rotateLeft(node);
@@ -231,9 +369,9 @@ public class SplayBST<T extends Comparable<T>> extends AbstractSet<T> {
 
 
     // Визуализация
-    public void printTree() {
-        int maxLevel = maxLevel(root);
-        printTree(Collections.singletonList(root), 1, maxLevel);
+    public void printTree(Node<T> n) {
+        int maxLevel = maxLevel(n);
+        printTree(Collections.singletonList(n), 1, maxLevel);
     }
 
     private static <T extends Comparable<?>> void printTree(List<Node<T>> nodes, int level, int maxLevel) {
--- src/test/java/TestSplayBST.java
+++ src/test/java/TestSplayBST.java
@@ -1,74 +1,154 @@
 import org.junit.jupiter.api.Test;
+import java.util.AbstractSet;
+import static org.junit.Assert.*;
 
 class TestSplayBST {
+        AbstractSet<Integer> actual = new SplayBST<>();
+        SplayBST<Integer> control = new SplayBST<>();
 
         @Test
-        void splayTree() {
-                SplayBST<Integer> actual = new SplayBST<>();
-                SplayBST<Integer> control = new SplayBST<>();
+        void testAddAddWithSplitContainsFind() {
+                actual = new SplayBST<>();
+                control = new SplayBST<>();
 
-                actual.add(15);
-                actual.add(3);
-                actual.add(2);
-                actual.add(5);
-                actual.add(10);
-                actual.add(1);
+                assertTrue(actual.add(5));
+                assertTrue(actual.add(15));
+                assertTrue(actual.add(1));
+                assertTrue(actual.add(3));
+                assertTrue(actual.add(10));
+                assertTrue(actual.add(2));
+                assertEquals(actual.size(), 6);
+                assertFalse(actual.isEmpty());
 
+                assertTrue(actual.contains(5));
+                assertTrue(actual.contains(15));
+                assertTrue(actual.contains(10));
+                assertTrue(actual.contains(2));
+
+                assertFalse(actual.contains(6));
+                assertFalse(actual.contains(7));
+                assertFalse(actual.contains(0));
+                assertFalse(actual.contains(16));
+
+                assertTrue(control.addWithSplit(1));
+                assertTrue(control.addWithSplit(15));
+                assertTrue(control.addWithSplit(3));
+                assertTrue(control.addWithSplit(5));
+                assertTrue(control.addWithSplit(10));
+                assertTrue(control.addWithSplit(2));
+                assertEquals(control.size(), 6);
+                assertFalse(control.isEmpty());
+
+                assertTrue(control.contains(5));
+                assertTrue(control.contains(10));
+                assertTrue(control.contains(2));
+                assertTrue(control.contains(3));
+
+                assertFalse(control.contains(6));
+                assertFalse(control.contains(7));
+                assertFalse(control.contains(0));
+                assertFalse(control.contains(16));
+
+                assertEquals(actual, control);
+                assertTrue(control.contains(15));
+                assertEquals(control.getRootValue(), 15);
+                assertTrue(control.contains(2));
+                assertEquals(control.getRootValue(), 2);
+        }
+
+
+        @Test
+        void testRemoveRemoveWitSplit() {
+                actual = new SplayBST<>();
+                control = new SplayBST<>();
+
+                assertTrue(actual.add(5));
+                assertTrue(actual.add(15));
+                assertTrue(actual.add(1));
+                assertTrue(actual.add(3));
+                assertTrue(actual.add(10));
+                assertTrue(actual.add(2));
+
+                assertTrue(control.addWithSplit(1));
+                assertTrue(control.addWithSplit(15));
+                assertTrue(control.addWithSplit(3));
+                assertTrue(control.addWithSplit(5));
+                assertTrue(control.addWithSplit(10));
+                assertTrue(control.addWithSplit(2));
+
+                assertEquals(actual, control);
+                assertFalse(actual.remove(0));
+                assertTrue(actual.contains(5));
+                assertTrue(actual.remove(5));
+                assertFalse(actual.remove(5));
+                assertFalse(actual.contains(5));
+
+                assertNotEquals(actual, control);
+                assertTrue(actual.remove(1));
+                assertFalse(actual.remove(1));
+                assertTrue(actual.remove(15));
+                assertFalse(actual.remove(15));
+                assertEquals(actual.size(), 3);
+                assertFalse(actual.isEmpty());
+
+                assertTrue(actual.remove(2));
+                assertTrue(actual.remove(3));
+                assertTrue(actual.remove(10));
+                assertTrue(actual.isEmpty());
+
+                assertFalse(control.removeWithSplit(0));
+                assertTrue(control.contains(5));
+                assertTrue(control.removeWithSplit(5));
+                assertFalse(control.removeWithSplit(5));
+                assertFalse(control.contains(5));
+
+                assertNotEquals(actual, control);
+                assertTrue(control.removeWithSplit(1));
+                assertFalse(control.removeWithSplit(1));
+                assertTrue(control.removeWithSplit(15));
+                assertFalse(control.removeWithSplit(15));
+                assertEquals(control.size(), 3);
+                assertFalse(control.isEmpty());
+
+                assertTrue(control.remove(2));
+                assertTrue(control.remove(3));
+                assertTrue(control.remove(10));
+                assertTrue(control.isEmpty());
+                assertEquals(actual, control);
+        }
+
+        @Test
+        void testSplitMergeFindMaxFindMinVisualization() {
+                control = new SplayBST<>();
+
+                control.add(10);
                 control.add(1);
                 control.add(15);
-                control.add(3);
                 control.add(5);
-                control.add(10);
                 control.add(2);
+                control.add(3);
+                control.printTree(control.root);
 
-                System.out.println("actual:");
-                actual.printTree();
-                System.out.println("control:");
-                control.printTree();
-                System.out.println("actual == control = " + (actual == control));
+                control.root = control.findMax(control.root);
+                System.out.println("fMax=" + control.root.value);
+                System.out.println("control=");
+                control.printTree(control.root);
 
-                System.out.println();
-                System.out.println("actual.size() = " + actual.size());
-                System.out.println("actual.contains(10) = " + actual.contains(10));
-                System.out.println("actual после contains(10):");
-                actual.printTree();
-                System.out.println("control:");
-                control.printTree();
-                System.out.println("actual == control = " + (actual == control));
+                control.root = control.findMin(control.root);
+                System.out.println("fMin=" + control.root.value);
+                System.out.println("control=");
+                control.printTree(control.root);
 
-                System.out.println();
-                actual.remove(5);
-                System.out.println("actual после удаления 5:");
-                actual.printTree();
-                System.out.println("actual == control = " + (actual == control));
 
-                System.out.println();
-                actual.remove(10);
-                System.out.println("actual после удаления 10:");
-                actual.printTree();
-                System.out.println("actual == control = " + (actual == control));
+                SplayBST.Pair res = control.split(5);
+                System.out.println("left side after split(5) =");
+                control.printTree(res.left);
+                System.out.println("right side after split(5) =");
+                control.printTree(res.right);
 
-                System.out.println();
-                actual.add(11);
-                actual.add(12);
-                System.out.println("actual после добавления 11 и 12:");
-                actual.printTree();
-                System.out.println("actual == control = " + (actual == control));
+                System.out.println("tree after merge =");
+                control.merge(res.left, res.right);
+                control.printTree(control.root);
 
-                System.out.println();
-                System.out.println("actual.contains(2) = " + actual.contains(2));
-                System.out.println("actual после contains(2):");
-                actual.printTree();
-                System.out.println("control:");
-                control.printTree();
-                System.out.println("control.size() = " + control.size());
-                System.out.println("actual == control = " + (actual == control));
-
-                System.out.println();
-                System.out.println("actual.contains(12) = " + actual.contains(12));
-                System.out.println("actual.add(12) = " + actual.add(12));
-                System.out.println("actual после contains(12):");
-                actual.printTree();
-                System.out.println("actual.size() = " + actual.size());
         }
 }
